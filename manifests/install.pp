c0a4ddc02adca3dd93c4d8458b4affd53852858b
ryefig <ryefig@users.noreply.github.com>
2025-04-16 18:39:15 +0200

    chores: fix indendation

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,8 +5,8 @@ class sentinelone_agent::install {
   if $sentinelone_agent::manage_package {
     $pkg_req = Package['sentinelone_agent_package']
     package { 'sentinelone_agent_package':
-      ensure => $sentinelone_agent::package_ensure,
-      name   => $sentinelone_agent::package_name,
+      ensure          => $sentinelone_agent::package_ensure,
+      name            => $sentinelone_agent::package_name,
       install_options => $sentinelone_agent::package_install_options,
     }
   } else {
